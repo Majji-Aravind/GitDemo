29ab41b3a8f847b6427903feff91312ca386ebf1
Majji-Aravind <raja.aravind60@gmail.com>
2020-07-02 11:00:46 +0530

    first commit

---
+++ src/test/java/Testcases/AravindTest.java
@@ -0,0 +1,14 @@
+package Testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AravindTest {
+
+	@Test
+	public void ImTest()
+	{
+		System.out.println("Hello Testing world");
+		Assert.assertTrue(false);
+	}
+}
---
+++ src/test/java/Testcases/PechuDoingTest.java
@@ -0,0 +1,19 @@
+package Testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PechuDoingTest {
+
+	@Test
+	public void doTest()
+	{
+		System.out.println("Nenu manishine");
+		Assert.assertTrue(false);
+	}
+	@Test
+	public void Testdone()
+	{
+		System.out.println("Testing is over and out");
+	}
+}
